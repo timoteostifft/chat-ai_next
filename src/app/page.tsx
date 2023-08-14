c30cf1e1bc973e6aab0addf3c28327ab9ed4b8ed
timoteostifft <timoteostifft@gmail.com>
2023-08-14 08:31:57 -0300

    feat: add main page base styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <Card className="w-[440px] h-[700px]">
+        <CardHeader>
+          <CardTitle>Chat AI</CardTitle>
+          <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Messages</p>
+        </CardContent>
+        <CardFooter>
+          form
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
