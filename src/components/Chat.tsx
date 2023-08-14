c7ac1c0e59ac0cbadfbcc05e34d1c2f1b65034b3
timoteostifft <timoteostifft@gmail.com>
2023-08-14 09:46:04 -0300

    style: add scroll area component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import { Button } from "../components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
 import { Input } from "../components/ui/input";
+import { ScrollArea } from "./ui/scroll-area";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
@@ -12,40 +13,40 @@ export default function Chat() {
   })
 
   return (
-    <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
+    <Card className="w-[440px]">
         <CardHeader>
           <CardTitle>Chat AI</CardTitle>
           <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">
-          { messages.map(message => {
-            return (
-              <div key={message.id} className="flex gap-3 text-slate-600 text-sm">
-                { message.role === 'user' && (
-                  <Avatar>
-                    <AvatarFallback>TS</AvatarFallback>
-                    <AvatarImage src="https://github.com/timoteostifft.png"/>
-                  </Avatar>
-                )}
+        <CardContent>
+          <ScrollArea className="h-[600px] w-full pr-4">
+            { messages.map(message => {
+              return (
+                <div key={message.id} className="flex gap-3 text-slate-600 text-sm mb-4">
+                  { message.role === 'user' && (
+                    <Avatar>
+                      <AvatarFallback>TS</AvatarFallback>
+                      <AvatarImage src="https://github.com/timoteostifft.png"/>
+                    </Avatar>
+                  )}
 
-                { message.role === 'assistant' && (
-                  <Avatar>
-                    <AvatarFallback>AI</AvatarFallback>
-                    <AvatarImage src="https://github.com/vercel-labs.png"/>
-                  </Avatar>
-                )}
+                  { message.role === 'assistant' && (
+                    <Avatar>
+                      <AvatarFallback>AI</AvatarFallback>
+                      <AvatarImage src="https://github.com/vercel-labs.png"/>
+                    </Avatar>
+                  )}
 
-                <p className="leading-relaxed">
-                  <span className="block font-bold text-slate-700">
-                    { message.role === 'user' ? 'You' : 'AI' }
-                  </span>
-                  { message.content }
-               </p>
-            </div>
-            )
-          }) }
-
-          
+                  <p className="leading-relaxed">
+                    <span className="block font-bold text-slate-700">
+                      { message.role === 'user' ? 'You' : 'AI' }
+                    </span>
+                    { message.content }
+                </p>
+              </div>
+              )
+            })}
+          </ScrollArea>
         </CardContent>
         <CardFooter>
           <form className="w-full flex gap-2" onSubmit={handleSubmit}>
