002e7971b05e08722505eb97b22f71eb92701d01
timoteostifft <timoteostifft@gmail.com>
2023-08-14 09:40:45 -0300

    feat: add openai configuration

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,11 +1,16 @@
 'use client'
 
+import { useChat } from "ai/react";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import { Button } from "../components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
 import { Input } from "../components/ui/input";
 
 export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: '/api/chat'
+  })
+
   return (
     <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
         <CardHeader>
@@ -13,31 +18,40 @@ export default function Chat() {
           <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          <div className="flex gap-3 text-slate-600 text-sm">
-            <Avatar>
-              <AvatarFallback>TS</AvatarFallback>
-              <AvatarImage src="https://github.com/timoteostifft.png"/>
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-700">You</span>
-              What is your opinion about javascript?
-            </p>
-          </div>
+          { messages.map(message => {
+            return (
+              <div key={message.id} className="flex gap-3 text-slate-600 text-sm">
+                { message.role === 'user' && (
+                  <Avatar>
+                    <AvatarFallback>TS</AvatarFallback>
+                    <AvatarImage src="https://github.com/timoteostifft.png"/>
+                  </Avatar>
+                )}
 
-          <div className="flex gap-3 text-slate-600 text-sm">
-            <Avatar>
-              <AvatarFallback>AI</AvatarFallback>
-              <AvatarImage src="https://github.com/vercel-labs.png"/>
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-700">AI</span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quisquam ipsa. Obcaecati laborum esse recusandae. Voluptas recusandae culpa error soluta, nemo officiis id maxime, est, reiciendis ducimus architecto rerum vitae.
-            </p>
-          </div>
+                { message.role === 'assistant' && (
+                  <Avatar>
+                    <AvatarFallback>AI</AvatarFallback>
+                    <AvatarImage src="https://github.com/vercel-labs.png"/>
+                  </Avatar>
+                )}
+
+                <p className="leading-relaxed">
+                  <span className="block font-bold text-slate-700">
+                    { message.role === 'user' ? 'You' : 'AI' }
+                  </span>
+                  { message.content }
+               </p>
+            </div>
+            )
+          }) }
+
+          
         </CardContent>
-        <CardFooter className="space-x-2">
-          <Input placeholder="How can I help you?"/>
-          <Button>Send</Button>
+        <CardFooter>
+          <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+            <Input placeholder="How can I help you?" value={input} onChange={handleInputChange}/>
+            <Button>Send</Button>
+          </form>
         </CardFooter>
       </Card>
   )
